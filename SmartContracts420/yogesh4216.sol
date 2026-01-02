9f5eecbc285281db82d84814c44cd026837c6996
yogesh4216 <yogesh2005m@gmail.com>
2026-01-03 00:21:04 +0530

    Added Hero of the Village ERC721 smart contract

---
+++ SmartContracts420/yogesh4216.sol
@@ -0,0 +1,26 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.20;
+
+import "@openzeppelin/contracts/token/ERC721/ERC721.sol";
+import "@openzeppelin/contracts/access/Ownable.sol";
+
+contract HeroOfTheVillage is ERC721, Ownable {
+
+    constructor() ERC721("HeroOfTheVillage", "HERO") {}
+
+    // Mint (Deposit) NFT
+    function deposit(address to, uint256 tokenId) public onlyOwner {
+        _safeMint(to, tokenId);
+    }
+
+    // Withdraw NFT (transfer back to owner)
+    function withdraw(uint256 tokenId) public {
+        require(ownerOf(tokenId) == msg.sender, "Not token owner");
+        _transfer(msg.sender, owner(), tokenId);
+    }
+
+    // Get balance of NFTs
+    function getBalance(address ownerAddress) public view returns (uint256) {
+        return balanceOf(ownerAddress);
+    }
+}
